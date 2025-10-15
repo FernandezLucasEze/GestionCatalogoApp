67f9dc8e9583cfc4042977482b252ea0c6b5af7d
Lucas Ezequiel Fernandez <fernandezlucaseze@gmail.com>
2025-10-15 00:22:54 -0300

    chore: Update gitignore and remove backup folder

--- src/Presentation/GestionCatalogo.Web_backup/GestionCatalogo.Web/Services/IAuthService.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace GestionCatalogo.Web.Services;
-public interface IAuthService
-{
-    Task<bool> RegisterAsync(RegisterDto registerDto);
-    Task<LoginResponseDto?> LoginAsync(LoginDto loginDto);
-}
-public class RegisterDto { public string Username { get; set; } = ""; public string Password { get; set; } = ""; }
-public class LoginDto { public string Username { get; set; } = ""; public string Password { get; set; } = ""; }
-public class LoginResponseDto { public string Token { get; set; } = ""; }
